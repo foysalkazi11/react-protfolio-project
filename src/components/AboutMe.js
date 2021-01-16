1c6e65a863f5aa778ea439b2f221251205c19e60
foysalkazi11 <foysalkazi11@gmail.com>
2021-01-16 12:53:09 +0600

    correct speling mistake

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -18,7 +18,7 @@ const AboutMe = () => {
             </p>
             <p>
               I am self-taught web developer. Focusing on React I am also
-              learning MERN stack development. My goal is to land entry/mediub
+              learning MERN stack development. My goal is to land entry/medium
               level front-end/back-end web development job based on my skills
               and grow my career as a full-stack javaScript developer.
               <br /> Web development has never been “just a earning source” for
