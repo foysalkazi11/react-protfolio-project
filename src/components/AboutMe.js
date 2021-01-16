7e6e32c8981cfb69e6760087472f63d9c7311e59
foysalkazi11 <foysalkazi11@gmail.com>
2021-01-16 12:50:13 +0600

    modify about-me section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -18,9 +18,9 @@ const AboutMe = () => {
             </p>
             <p>
               I am self-taught web developer. Focusing on React I am also
-              learning MERN stack development. My goal is to land entry level
-              front-end/back-end web development job based on my skills. Then
-              grow my career as a web developer.
+              learning MERN stack development. My goal is to land entry/mediub
+              level front-end/back-end web development job based on my skills
+              and grow my career as a full-stack javaScript developer.
               <br /> Web development has never been “just a earning source” for
               me, it’s always offer me engaging challenge to continuously learn
               and improve my skills. Now it’s become a fully-fledged passion
