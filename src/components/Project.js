80010c30fee26cd3d6a0939d17bf9aacbf4233a2
foysalkazi11 <80021202+foysalkazi@users.noreply.github.com>
2022-07-08 00:43:56 +0600

    change protfolio

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import Popup from "./Popup";
 import yourMoment from "../images/project pic/your-moments.png";
-// import cocktaildbApiProject from "../images/project pic/cocktaildb-api-project.png";
-// import cloudServerSetup from "../images/project pic/cloud server setup.png";
-// import marketing from "../images/project pic/marketing.png";
+//import cocktaildbApiProject from "../images/project pic/cocktaildb-api-project.png";
+import cloudServerSetup from "../images/project pic/cloud server setup.png";
+import marketing from "../images/project pic/marketing.png";
 // import mzEcom from "../images/project pic/MzEcom.png";
 import furniture from "../images/project pic/Jenni-Kayne.png";
-import reactMui from "../images/project pic/reactMui.png";
-import bimaXpress from "../images/project pic/bimaXpress.png";
+//import reactMui from "../images/project pic/reactMui.png";
+//import bimaXpress from "../images/project pic/bimaXpress.png";
 import { FaSearch } from "react-icons/fa";
 const projectDetails = [
   // {
@@ -25,14 +25,15 @@ const projectDetails = [
     demoUrl: "https://your-moments.netlify.app/",
     githubUrl: "https://github.com/foysalkazi11/your-moments",
   },
-  {
-    title: "BimaXpress",
-    img: bimaXpress,
-    technologyUse: "React, TypeScript, Tailwindcss",
-    demoUrl: "https://heuristic-heyrovsky-c7b3a2.netlify.app/",
-    githubUrl:
-      "https://github.com/foysalkazi11/bimaXpress_react_tailwind_typescript",
-  },
+
+  // {
+  //   title: "BimaXpress",
+  //   img: bimaXpress,
+  //   technologyUse: "React, TypeScript, Tailwindcss",
+  //   demoUrl: "https://heuristic-heyrovsky-c7b3a2.netlify.app/",
+  //   githubUrl:
+  //     "https://github.com/foysalkazi11/bimaXpress_react_tailwind_typescript",
+  // },
   {
     title: "Jenni Kayne",
     img: furniture,
@@ -42,19 +43,23 @@ const projectDetails = [
     githubUrl: "https://github.com/foysalkazi11/mern-furniture-store",
   },
   {
-    title: "MUI-Design",
-    img: reactMui,
-    technologyUse: "React, Material UI",
-    demoUrl: "https://priceless-kare-1b24ef.netlify.app/",
-    githubUrl: "https://github.com/foysalkazi11/react-material-mobile-design",
+    title: "Cloud Server Setup",
+    img: cloudServerSetup,
+    technologyUse:
+      "React,React-router-dom,Framer-motion,React-modal,React-slick,styled-componets,tailwindcss",
+    demoUrl: "https://serverify-572b3.web.app/",
+    githubUrl: "https://github.com/mztechcorp/template_Cloud_Server_Setup",
   },
-  // {
-  //   title: "Cloud Server Setup",
-  //   img: cloudServerSetup,
-  //   technologyUse:"React,React-router-dom,Framer-motion,React-modal,React-slick,styled-componets,tailwindcss",
-  //   demoUrl: "https://serverify-572b3.web.app/",
-  //   githubUrl: "https://github.com/mztechcorp/template_Cloud_Server_Setup"
-  // },
+
+  {
+    title: "Marketing",
+    img: marketing,
+    technologyUse:
+      "React,React-router-dom,Framer-motion,React-modal,React-slick,styled-componets,tailwindcss",
+    demoUrl: "https://tradezilla-3da7a.web.app/",
+    githubUrl: "https://github.com/mztechcorp/template_marketing",
+  },
+
   // {
   //   title: "CocktailDB Api",
   //   img: cocktaildbApiProject,
@@ -62,13 +67,13 @@ const projectDetails = [
   //   demoUrl: "https://gifted-varahamihira-d66b31.netlify.app/",
   //   githubUrl: "https://github.com/foysalkazi11/react-cocktail-api-project"
   // },
+
   // {
-  //   title: "React - Marketing Team",
-  //   img: marketing,
-  //   technologyUse:
-  //     "React,React-router-dom,Framer-motion,React-modal,React-slick,styled-componets,tailwindcss",
-  //   demoUrl: "https://tradezilla-3da7a.web.app/",
-  //   githubUrl: "https://github.com/mztechcorp/template_marketing"
+  //   title: "MUI-Design",
+  //   img: reactMui,
+  //   technologyUse: "React, Material UI",
+  //   demoUrl: "https://priceless-kare-1b24ef.netlify.app/",
+  //   githubUrl: "https://github.com/foysalkazi11/react-material-mobile-design",
   // },
 ];
 
